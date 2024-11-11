3da370392201e0b65f29f7238728688473b2e57f
Nadir Marsiglia <159026044+NadirMB26@users.noreply.github.com>
2024-11-11 18:23:22 -0500

    Add files via upload

---
+++ Taller18/Ejercicio1/Prueba.java
@@ -0,0 +1,15 @@
+package Taller18.Ejercicio1;
+
+public class Prueba {
+    public static void main(String[] args) {
+        // Crear una instancia de ServicioMantenimiento
+        ServicioMantenimiento servicio = new ServicioMantenimiento();
+
+        // Realizar reparaciones
+        servicio.reparar();
+
+        // Realizar limpieza
+        servicio.limpiar();
+    }
+}
+
